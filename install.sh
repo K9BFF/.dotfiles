01e1c32f806fbbfd43ab4d3b861b264dd57bdc6c
K9BFF <masoncodes0@gmail.com>
2025-11-11 13:06:40 -0600

    added better thangz

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-echo "dots installer"
+# This script is mainly intended for my own personal use. This may not work out of the box for you!!
+
+echo "dotfile installer"
 echo
 echo "congrats on the reinstall dumbass."
 read -p "this is gonna overwrite all your configs btw is that ok? (y/N) " yesno
@@ -13,6 +15,11 @@ if [[ ${yesno:0:1} == "y" ]]; then
 	cp waybar/ ~/.config/
 	cp nvim/ ~/.config/
 	sudo cp nixos/* /etc/nixos/
+	echo "done."
+	echo "adding update-dotfiles to ~/.local/bin..."
+	mv update.sh update-dotfiles
+	chmod +x update-dotfiles
+	mv update-dotfiles ~/.local/bin
 	echo "done."
 else
 	echo "no worries"
--- update.sh
+++ update.sh
@@ -1,19 +1,21 @@
 #!/usr/bin/env bash
 
-echo "dots updater"
+# This script is mainly intended for my own personal use. This may not work out of the box for you!!
+
+echo "updating dotfiles..."
 echo
 
-cp ~/.config/dunst/dunstrc dunst/
-cp ~/.config/fish/config.fish fish/
-cp ~/.config/hypr/hyprland.conf hypr/
-cp ~/.config/kitty/kitty.conf kitty/
-cp ~/.config/rofi/config.rasi rofi/
-cp ~/.config/waybar/config waybar/
-cp ~/.config/nvim/init.vim nvim/
-sudo cp /etc/nixos/* nixos/
+cp ~/.config/dunst/dunstrc ~/.dotfiles/dunst/
+cp ~/.config/fish/config.fish ~/.dotfiles/fish/
+cp ~/.config/hypr/hyprland.conf ~/.dotfiles/hypr/
+cp ~/.config/kitty/kitty.conf ~/.dotfiles/kitty/
+cp ~/.config/rofi/config.rasi ~/.dotfiles/rofi/
+cp ~/.config/waybar/config ~/.dotfiles/waybar/
+cp ~/.config/nvim/init.vim ~/.dotfiles/nvim/
+sudo cp /etc/nixos/* ~/.dotfiles/nixos/
 
-echo "updated. pushing to git..."
+echo "updated locally. pushing to git..."
 git add *
-git commit -m "updated"
+git commit -m "updated dotfiles"
 git push -u origin main
-echo "done."
+echo "all done!"
